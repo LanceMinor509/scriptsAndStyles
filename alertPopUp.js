88dcf0aea0bf40a1bbf4d71ed88e22b60fcb48fb
lance <lance.minor1@gmail.com>
2019-05-25 10:59:49 -0400

    first

---
+++ alertPopUp.js
@@ -0,0 +1,25 @@
+function alerts(p){
+    var b = document.createElement("div");
+    b.setAttribute("class","alertBox")
+    var x = document.createElement("span");
+    x.setAttribute("class","closebtn");
+    x.setAttribute("onclick","this.parent.style.display = 'none'");
+
+
+    switch(p.toLowerCase()){
+        case "d":
+            
+        break;
+        case "s":
+
+        break;
+        case "i":
+
+        break;
+        case "w":
+
+        break;
+        default:
+
+    }
+}
