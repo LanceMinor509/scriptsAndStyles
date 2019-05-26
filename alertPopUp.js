44c3548da01bdbb9eb2444ed051e0aeef9b7f03b
lance <lance.minor1@gmail.com>
2019-05-25 21:55:55 -0400

    automatic upload

--- alertPopUp.js
+++ alertPopUp.js
@@ -1,4 +1,5 @@
 function alerts(p,text){
+    var msg = "";
     //wrapper
     var b = document.createElement("div");
     b.style.padding = "20px";
@@ -7,29 +8,35 @@ function alerts(p,text){
     b.style.top = "0";
     b.style.left = "0";
     b.style.width = "100%";
+    b.style.zIndex = "9999999";
 
     switch(p.toLowerCase()){
         case "d":
-            b.style.backgroundColor = "#F44336";    
+            b.style.backgroundColor = "#F44336";  
+            msg = "Danger!";  
         break;
         case "s":
             b.style.backgroundColor = "#4CAF50";
+            msg = "Success!";
         break;
         case "i":
             b.style.backgroundColor = "#2196F3";
+            msg = "Info!";
         break;
         case "w":
             b.style.backgroundColor = "#FF9800";
+            msg = "Warning!";
         break;
         default:
             b.style.backgaroundColor = "#2196F3";
             p = "i"
+            msg = "Info!";
     }
    
     //text
     var textE = document.createElement("span");
     textE.color = "#FFFFFF";
-    textE.innerHTML = text;
+    textE.innerHTML = "<span style='font-weight: bold;margin-right: 10px'>" + msg + "</span>" + text;
     
     //exit button
     var x = document.createElement("span");
@@ -39,14 +46,15 @@ function alerts(p,text){
     x.style.float = "right";
     x.style.lineHeight = "20px";
     x.style.cursor = "pointer";
-    x.setAttribute("onclick","this.parent.style.display = 'none'");
-    x.innerText = "&times;";
+    x.style.position = "fixed";
+    x.style.right = "30px";
+    x.onclick = function(){
+        b.style.display = "none";
+    }
+    x.innerHTML = "&times;";
 
     document.body.appendChild(b);
     b.appendChild(textE);
     b.appendChild(x);
 
 }
-
-console.log("%calert loaded!","color:yellow");
-
